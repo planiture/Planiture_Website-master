0e3189a552f7454fb30fd6b6652eacb9ebd527b2
kingz will <kingzwill41@gmail.com>
2020-10-04 19:42:58 -0500

    designing mail system in admin system

---
+++ Planiture_Website/Areas/Identity/Pages/Admin/Inbox.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Planiture_Website.Areas.Identity.Pages.Admin
+{
+    public class InboxModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
